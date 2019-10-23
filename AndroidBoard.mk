dd3d3cc7d07c16f242f1844c744b5d6fa3589cc3
Mathias <30691126+undevdecatos@users.noreply.github.com>
2019-10-23 14:45:23 +0200

    Update AndroidBoard.mk

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -3,4 +3,4 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 # include the non-open-source counterpart to this file
--include vendor/wiko/p6601/AndroidBoardVendor.mk
+-include vendor/wiko/p6609/AndroidBoardVendor.mk
